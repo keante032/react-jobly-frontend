cc58f33224a31d601ba4fd6f54d51d450ce35e10
Kevin Templeton <templeton.kevin@gmail.com>
2023-02-28 14:40:50 -0600

    rebuilt with react-router-bootstrap

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 /** Navigation bar that shows up on every page.
  *
@@ -9,26 +10,33 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <nav>
-            <Link to="/">Jobly</Link>
-            <ul>
-                <li>
-                    <NavLink to="/companies">Companies</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/jobs">Jobs</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">Login</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/signup">Signup</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/profile">Profile</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <Navbar bg="dark" variant="dark" expand="md">
+            <Container>
+                <LinkContainer to="/">
+                    <Navbar.Brand>Jobly</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <LinkContainer to="/companies">
+                            <Nav.Link>Companies</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/jobs">
+                            <Nav.Link>Jobs</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/signup">
+                            <Nav.Link>Signup</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/profile">
+                            <Nav.Link>Profile</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
