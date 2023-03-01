d70794b62bf2545acd8e3d92f5366414e54b757c
Kevin Templeton <templeton.kevin@gmail.com>
2023-03-01 03:08:02 -0600

    navbar items aligned right

--- src/Navigation.js
+++ src/Navigation.js
@@ -17,7 +17,7 @@ const Navigation = () => {
                 </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto">
                         <LinkContainer to="/companies">
                             <Nav.Link>Companies</Nav.Link>
                         </LinkContainer>
